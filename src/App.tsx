f329caa81c4da79dab0d854942adb62a6e49c9b7
memogarrido <guillermo@garrido.dev>
2022-04-27 01:05:11 +0200

    #7 Clear completed button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useAppDispatch } from "./app/hooks";
 import LayoutContainer from "./common/layout/layout-container/LayoutContainer";
 import CompletedTodosPage from "./features/todos/pages/done-todos-page/CompletedTodosPage";
 import TodosPage from "./features/todos/pages/todos-page/TodosPage";
+import { getTodosAsync } from "./features/todos/store/todosSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getTodosAsync());
+  }, []);
   return (
     <BrowserRouter>
       <LayoutContainer>
